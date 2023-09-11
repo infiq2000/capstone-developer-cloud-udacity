6b3093123cefca187bae7f68c2d17c72e4df77d2
dat <datht@example.com>
2023-09-11 10:24:12 +0700

    fix

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -4,13 +4,13 @@ import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 import { getUserId } from '../utils'
-import { createTodo } from '../../businessLogic/todos'
+import { createStudent } from '../../businessLogic/todos'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newStudent: CreateTodoRequest = JSON.parse(event.body)
     const userId = getUserId(event)
-    const student = await createTodo(userId, newStudent)
+    const student = await createStudent(userId, newStudent)
     return {
       statusCode: 201,
       headers: {
